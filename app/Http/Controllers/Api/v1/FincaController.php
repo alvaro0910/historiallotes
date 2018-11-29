17d59c0a6c77af2a0eee9001fb4ebd07ff310378
Alvaro Castaño <alvarocastano6@gmail.com>
2018-11-29 17:14:13 -0500

    Hightcharts Api

--- app/Http/Controllers/Api/v1/FincaController.php
+++ app/Http/Controllers/Api/v1/FincaController.php
@@ -11,32 +11,35 @@ use App\Http\Requests\FincaRequest;
 
 class FincaController extends Controller
 {
-  public function index()
-  {
-      return Finca::all();
-  }
+    public function index()
+    {
+        //return Finca::all();
+        $arreglo = Finca::all();
+        return \Response::json(['fincas' => $arreglo, 'status' => '200'], 200);
+    }
 
-  public function show(Finca $finca)
-  {
-      return $finca;
-  }
+    public function show(Finca $finca)
+    {
+        return response()->json($finca, 200);
+        //return $finca;
+    }
 
-  public function store(FincaRequest $request)
-  {
-      $finca = new Finca($request->all());
-      $finca->save();
-      return response()->json($finca, 201);
-  }
+    public function store(FincaRequest $request)
+    {
+        $finca = new Finca($request->all());
+        $finca->save();
+        return response()->json($finca, 201);
+    }
 
-  public function update(FincaRequest $request, Finca $finca)
-  {
-      $finca->update($request->all());
-      return response()->json($finca, 200);
-  }
+    public function update(FincaRequest $request, Finca $finca)
+    {
+        $finca->update($request->all());
+        return response()->json($finca, 200);
+    }
 
-  public function delete(Finca $finca)
-  {
-      $finca->delete();
-      return response()->json(null, 204);
-  }
+    public function delete(Finca $finca)
+    {
+        $finca->delete();
+        return response()->json(null, 204);
+    }
 }
